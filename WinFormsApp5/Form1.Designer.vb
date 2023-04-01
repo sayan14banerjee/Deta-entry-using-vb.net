5eb0cc476950de58a5b66d6ed806e856a9cfdb20
SAYAN <SAYAN@SAYAN>
2023-04-01 20:55:19 +0100

    Add project files.

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
+        DataGridView1 = New DataGridView()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        GroupBox1.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.AppWorkspace
+        GroupBox1.Controls.Add(Button4)
+        GroupBox1.Controls.Add(Button3)
+        GroupBox1.Controls.Add(Button2)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(DataGridView1)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(17, 14)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(768, 427)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = SystemColors.ControlDarkDark
+        Button4.Cursor = Cursors.Hand
+        Button4.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Italic, GraphicsUnit.Point)
+        Button4.ForeColor = SystemColors.InfoText
+        Button4.Location = New Point(253, 350)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(82, 48)
+        Button4.TabIndex = 12
+        Button4.Text = "Delete"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ControlDarkDark
+        Button3.Cursor = Cursors.Hand
+        Button3.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Italic, GraphicsUnit.Point)
+        Button3.ForeColor = SystemColors.InfoText
+        Button3.Location = New Point(169, 350)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(79, 48)
+        Button3.TabIndex = 11
+        Button3.Text = "Update"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.ControlDarkDark
+        Button2.Cursor = Cursors.Hand
+        Button2.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Italic, GraphicsUnit.Point)
+        Button2.ForeColor = SystemColors.InfoText
+        Button2.Location = New Point(91, 350)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(73, 48)
+        Button2.TabIndex = 10
+        Button2.Text = "Show"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ControlDarkDark
+        Button1.Cursor = Cursors.Hand
+        Button1.Font = New Font("Arial Rounded MT Bold", 12F, FontStyle.Italic, GraphicsUnit.Point)
+        Button1.ForeColor = SystemColors.InfoText
+        Button1.Location = New Point(13, 350)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(73, 48)
+        Button1.TabIndex = 9
+        Button1.Text = "Save"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(346, 25)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(407, 316)
+        DataGridView1.TabIndex = 8
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label4.ForeColor = Color.Red
+        Label4.Location = New Point(7, 219)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(71, 25)
+        Label4.TabIndex = 7
+        Label4.Text = "Course"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label3.ForeColor = Color.Red
+        Label3.Location = New Point(6, 179)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 25)
+        Label3.TabIndex = 6
+        Label3.Text = "Address"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label2.ForeColor = Color.Red
+        Label2.Location = New Point(6, 138)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(65, 25)
+        Label2.TabIndex = 5
+        Label2.Text = "Name"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        Label1.ForeColor = Color.Red
+        Label1.Location = New Point(7, 97)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(29, 25)
+        Label1.TabIndex = 4
+        Label1.Text = "Id"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Font = New Font("Shonar Bangla", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        TextBox4.Location = New Point(91, 215)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(237, 29)
+        TextBox4.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Font = New Font("Shonar Bangla", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        TextBox3.Location = New Point(91, 175)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(237, 29)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Shonar Bangla", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        TextBox2.Location = New Point(91, 134)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(237, 29)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Shonar Bangla", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point)
+        TextBox1.Location = New Point(91, 93)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(237, 29)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ WinFormsApp5/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Public Class Form1
+    Sub view()
+        Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\College.mdb")
+        con.Open()
+        Dim adp As New OleDbDataAdapter("select * from Student", con)
+        Dim ds As New DataSet
+        adp.Fill(ds, "Student")
+        DataGridView1.DataSource = ds.Tables("Student")
+        con.Close()
+    End Sub
+
+    Public Sub ShowData(ByVal CurrentRow)
+        Try
+            TextBox1.Text = ds.Tables("Student").Rows(CurrentRow)("ID")
+            TextBox2.Text = ds.Tables("Student").Rows(CurrentRow)("Name")
+            TextBox3.Text = ds.Tables("Student").Rows(CurrentRow)("Address")
+            TextBox4.Text = ds.Tables("Student").Rows(CurrentRow)("Course")
+        Catch ex As Exception
+            MsgBox(ex.Message, "error")
+
+        End Try
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\College.mdb")
+        con.Open()
+        Dim cmd As New OleDbCommand()
+        cmd.Connection = con
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into Student values ( " & TextBox1.Text & ",' " & TextBox2.Text & " ', ' " & TextBox3.Text & " ', ' " & TextBox4.Text & " ')  "
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MsgBox("Data Save Successfully")
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox1.Focus()
+        view()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        view()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        view()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\College.mdb")
+        con.Open()
+        Dim cmd As New OleDbCommand()
+        cmd.Connection = con
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "update Student set name=  ' " & TextBox2.Text & " ',  address= ' " & TextBox3.Text & " ', course= ' " & TextBox4.Text & " ' where id = " & TextBox1.Text & " "
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MsgBox("Record update successfully")
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox1.Focus()
+        view()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\College.mdb")
+        con.Open()
+        Dim cmd As New OleDbCommand()
+        cmd.Connection = con
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from Student where id =" & TextBox1.Text & ""
+        cmd.ExecuteNonQuery()
+        con.Close()
+        MsgBox("Delete data successfully")
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox1.Focus()
+        view()
+
+    End Sub
+
+
+End Class
